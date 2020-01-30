ae68aa925c19f522fce0f61cd29efd7e66abe551
Brian Vanegas <brianvanegas@viajesaeromar.com>
2020-01-30 16:55:43 -0500

    get contenttypes

--- app/controllers/ContenttypeController.ts
+++ app/controllers/ContenttypeController.ts
@@ -1,8 +1,21 @@
 import {Controller, Get, PathParams, Post, BodyParams, QueryParams} from "@tsed/common";
+const { readdirSync } = require('fs')
 import Markdown from './MarkdownController'
 
 @Controller("/contenttype")
 export class ContenttypeController {
+  @Get()
+  getContentTypes(@QueryParams("language") language: string) {
+    let dir  = './content';
+    if (language) {
+      dir = dir + '/' + language
+    }
+    
+    return readdirSync(dir, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory() && dirent.name != 'spanish' && dirent.name != 'english')
+    .map(dirent => dirent.name)
+  }
+  
   @Get("/:contentType")
   getAllContent(@PathParams("contentType") contentType: string) {
     let posts = new Markdown('archetypes', '', contentType);
